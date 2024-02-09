5df7f716f1d220803f72a57459982f2214f2a9bd
Erik Isberg <erik@isberg.nu>
2024-02-09 19:47:16 +0100

    Update are-ai-poc.py

--- are-ai-poc.py
+++ are-ai-poc.py
@@ -17,43 +17,38 @@ max_date = datetime.date(max_year, 12, 31)
 st.title("Åre Destination AI-POC")
 st.subheader("Planera ditt besök i Åre med hjälp av AI")
 st.write("Berätta mer om din vistelse i Åre och ditt sällskap så hjälper vi dig att hitta rätt aktiviteter och upplevelser.")
-user_input = st.text_area("Berätta om din semester och vad ni vill göra i Åre...")
+user_input = st.text_area("Vilka är ni och vad vill ni hitta på?")
 st.markdown("""
 * _Aa_
 * _Aa_
 * _Aa_
 """, unsafe_allow_html=True)
 number_of_people = st.slider('Hur många personer är det i ert sällskap?', min_value=1, max_value=30)
-d = st.date_input(
+arrival_dates = st.date_input(
     "När kommer ni till Åre?",
     value=(jan_1, datetime.date(next_year, 1, 7)),
-    min_value=min_date,
-    max_value=max_date,
-    format="DD.MM.YYYY",
 )
-huvudfokus_semester = st.selectbox('Vad är huvudfokus för semestern?', ["Massa äventy och aktiviter", "Mysa och slappna av", "Semester med familj och småbarn", "Äldre personer som vill njuta av åre", "Romantisk par-weekend för kärleksparet"])
-st.divider()
-submit_button = st.button("Skicka")
+huvudfokus_semester = st.selectbox('Vad är huvudfokus för er semester?', ["Nöjen och äventyr", "Relax och avslappning", "Äldre människor som vill uppleva Åre", "Romantisk getaway med min partner", "Romantik"])
+submit_button = st.button('Hitta aktiviteter')
 
 # POST Request API
-def send_post_request(input_text, workout_length, number_of_people, arrival_date, huvudfokus_semester):
-    url = "https://api.retool.com/v1/workflows/49b9ad64-b79c-4433-8dbc-d591e1016fa7/startTrigger?workflowApiKey=retool_wk_8f83ae0a17bb46d0a45ee6382cece909"
+def send_post_request(input_text, number_of_people, arrival_dates, huvudfokus_semester):
+    url = "https://api.retool.com/v1/workflows/b61838af-a3b6-40ee-bbe1-15ea6bc2fbd6/startTrigger?workflowApiKey=retool_wk_548bed49009e4b1e8967211445ce07c9"
     headers = {'Content-Type': 'application/json'}
     data = {
         "body": input_text, 
-        "number_of_people": number_of_people, 
-        "arrival_date": arrival_date.strftime("%Y-%m-%d"),  # Assuming arrival_date is a datetime object
-        "huvudfokus_semester": huvudfokus_semester
+        "number_of_people": "Beskrivning från användare: " + str(number_of_people), 
+        "arrival_start_date": "Datum för ankomst: " + arrival_dates[0].strftime("%Y-%m-%d"),  
+        "arrival_and_end_date": "Datum för avfärd: " + arrival_dates[1].strftime("%Y-%m-%d"),  
+        "huvudfokus_semester": "Huvudkategori:" + huvudfokus_semester
     }
     
     with st.spinner('Vi letar aktiviteter och rekommendationer...'):
         response = requests.post(url, json=data, headers=headers)
-
+    
     if response.status_code == 200:
         response_data = response.json()
-
         data_field = response_data.get("data", "")
-        
         return data_field
     else:
         return "Error in API response"
@@ -61,7 +56,7 @@ def send_post_request(input_text, workout_length, number_of_people, arrival_date
 # Button Click
 if submit_button:
     if user_input:
-        data_field = send_post_request(user_input, workout_length, number_of_people, d[0], selected_theme)
+        data_field = send_post_request(user_input, number_of_people, arrival_dates, huvudfokus_semester)
         st.write("Response Data:")
         st.write(data_field)
     else:
